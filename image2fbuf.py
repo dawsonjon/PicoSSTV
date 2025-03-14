410b04100e0347c1b24c06504c0d9c0a49edb823
dawsonjon <jon@jondawson.org.uk>
2025-03-14 17:50:46 +0000

    Update image2fbuf.py
    Fix errors that occur with latest version of imageio.

--- image2fbuf.py
+++ image2fbuf.py
@@ -27,9 +27,8 @@ pixels = []
 for y in range(h):
   for x in range(w):
     r, g, b = im[y][x][:3]
-    color = ((r & 0xF8) << 8) | ((g & 0xFC) << 3) | b >> 3;
-    color = (color >> 8 | color << 8) & 0xffff;
-    #color &= 0xffff;
+    color = ((r & 0xF8) << 8) | ((g & 0xFC) << 3) | b >> 3
+    color = (color >> 8 | color << 8)
     pixels.append("0x%04x"%color)
 pixels = ",\n".join(pixels)
 contents = "static const uint16_t __in_flash() %s[] = {%s};"%(name, pixels)
